a3d6e7e00a486083630e81d19fb324b91db775a8
hoy <hoy@hoyui-iMac.local>
2021-02-21 22:45:24 +0900

    create table && model fix

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.conf import settings
 from backend.djangoapps.main.views import UserViewSet
+
 urlpatterns = [
-    path("v1/user", UserViewSet.as_view({"get": "list", "post": "add"}), name="user"),
+    path("v1/user", UserViewSet.as_view({"post": "add", "put": "list", "delete": "del", "get": "list"}), name="user"),
     # path("v1/music/<int:music_num>", UserViewSet.as_view({"get": "list"}), name="music"),
 ]
